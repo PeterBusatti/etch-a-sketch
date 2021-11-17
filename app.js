a8a446bed0aac3f0ac5b52b896c266284d8fe99d
Peter Busatti <peterbusatti@gmail.com>
2021-11-17 17:30:04 -0500

    add functionality to grid size slider and color dropdown
    Added functions to change the grid size based on the slider, this also calls another function
    to clear the grid divs and event listeners and initializes a new play grid.
    Added functions to change the color changing aspect through the drop down. this funtion
    does not refresh the play grid, as to give functionality to the eraser.

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const container = document.getElementById("middle-main-container");
-let testNum = 16; //will be user selection later
+const colorSelect = document.getElementById("colorSelect");
+const gridSelect = document.getElementById("gridSelect");
+const gridOutputNum = document.getElementById("gridOutputNum");
 
+// setting page load values
+let color = "black";
+colorSelect.value = "black";
+gridSelect.value = 30;
+
+let squareNum = gridSelect.value;
+let individualBox = [];
 
 function playGridCreate(base) {
     let gridCount = base * base;
-    createSquares(gridCount);
+    createSquares(gridCount); 
+    individualBox = Array.from(document.getElementsByClassName("box")); 
+    individualBox.forEach(box => box.addEventListener("mouseenter", passChanger));
+    let output = document.createElement('p');
+    output.textContent = squareNum + "x" + squareNum;
+    gridOutputNum.appendChild(output);
 }
 
 function createSquares(num) {
-    const boxHeight = (document.getElementById("middle-main-container").clientHeight / testNum) + "px";
-    const boxWidth = (document.getElementById("middle-main-container").clientWidth / testNum) + "px";
+    const boxHeight = (document.getElementById("middle-main-container").clientHeight / squareNum) + "px";
+    const boxWidth = (document.getElementById("middle-main-container").clientWidth / squareNum) + "px";
     for (let i = 1; i <= num; i++) {
         const box = document.createElement("div");
         box.className = "box";
@@ -19,17 +33,41 @@ function createSquares(num) {
     }
 }
 
-playGridCreate(testNum);
-
-const individualBox = Array.from(document.getElementsByClassName("box"));
-
-individualBox.forEach(box => box.addEventListener("mouseenter", passChanger) );
+playGridCreate(squareNum);
 
 function passChanger(e) {
-    if(this.classList.contains("box")) {
-        e.target.className = "box-first-pass";
+    if(color === "black") {
+        e.target.style.backgroundColor = "black";
     }
-    /*else if(this.classList.contains("box-first-pass")) {
-        e.target.className = "box-second-pass";
-    }*/
+    else if (color === "rainbow") {
+        e.target.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+    }
+    else if (color === "eraser") {
+        e.target.style.backgroundColor = "#a5a5a5"
+    }
 }
+
+colorSelect.addEventListener("change", changeColor);
+
+function changeColor() {
+    color = this.value;
+    console.log(color);
+}
+
+gridSelect.addEventListener("change", changeGridValue);
+
+function changeGridValue() {
+    refresh();
+    squareNum = this.value;
+    playGridCreate(squareNum);
+}
+
+function refresh () {
+    gridOutputNum.removeChild(gridOutputNum.firstChild);
+    individualBox.forEach(box => box.removeEventListener("mouseenter", passChanger));
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    individualBox = [];
+}
+
