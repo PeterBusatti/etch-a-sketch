be6ccdcce77079528755e9f9cc280d4a95b58102
Peter Busatti <peterbusatti@gmail.com>
2021-11-17 13:36:31 -0500

    update the container div name in js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const container = document.getElementById("container");
+const container = document.getElementById("middle-main-container");
 let testNum = 16; //will be user selection later
 
 
@@ -8,8 +8,8 @@ function playGridCreate(base) {
 }
 
 function createSquares(num) {
-    const boxHeight = (document.getElementById("container").clientHeight / testNum) + "px";
-    const boxWidth = (document.getElementById("container").clientWidth / testNum) + "px";
+    const boxHeight = (document.getElementById("middle-main-container").clientHeight / testNum) + "px";
+    const boxWidth = (document.getElementById("middle-main-container").clientWidth / testNum) + "px";
     for (let i = 1; i <= num; i++) {
         const box = document.createElement("div");
         box.className = "box";
@@ -29,7 +29,7 @@ function passChanger(e) {
     if(this.classList.contains("box")) {
         e.target.className = "box-first-pass";
     }
-    else if(this.classList.contains("box-first-pass")) {
+    /*else if(this.classList.contains("box-first-pass")) {
         e.target.className = "box-second-pass";
-    }
+    }*/
 }
